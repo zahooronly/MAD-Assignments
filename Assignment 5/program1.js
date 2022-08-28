e4379ab2d76a6bdd8ae2901df8ac4400032b57c4
ZahoorOnly <i.am.zahoor.a@outlook.com>
2022-08-28 23:18:03 +0500

    Assignment 5

---
+++ Assignment 5/program1.js
@@ -0,0 +1,9 @@
+"use strict";
+let num1 = Number(prompt("Enter a number"));
+let num2 = Number(prompt("Enter another number"));
+if (num1 > num2) {
+    console.log(`${num1} is greater than ${num2}`);
+}
+else {
+    console.log(`${num2} is greater than ${num1}`);
+}
