b211857c3b12a24f3c9cb5ea0a384b1dc75659b7
Zahoor Ahmad <105358562+zahooronly@users.noreply.github.com>
2022-09-10 18:28:57 +0500

    updated notes

--- Assignment 5/Program21/app.js
+++ Assignment 5/Program21/app.js
@@ -1,9 +1,10 @@
 // Write a ts program to count total number of notes in given amount
-var notes = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-var amount = Number(prompt("Enter amount"));
-var count = 0;
-for (var i = 0; i < notes.length; i++) {
-    count += Math.floor(amount / notes[i]);
+"use strict";
+let notes = [5000, 1000, 500, 100, 50, 20, 10, 5, 2, 1];
+let amount = Number(prompt("Enter amount"));
+let numberOfNotes = 0;
+for (let i = 0; i < notes.length; i++) {
+    numberOfNotes += Math.floor(amount / notes[i]);
     amount = amount % notes[i];
 }
-console.log(count);
+console.log(numberOfNotes);
